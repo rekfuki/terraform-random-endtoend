cb103eb514297feabcccf9760c9180085e564b06
Andrius <anavasaitis@gmail.com>
2025-10-31 10:59:14 +0200

    test

--- main.tf
+++ main.tf
@@ -28,11 +28,7 @@ resource "null_resource" "always_run" {
   }
 }
 
-resource "null_resource" "always_run_2" {
-  triggers = {
-    timestamp = "${timestamp()}"
-  }
-}
+
 
 resource "random_password" "password" {
   length           = var.length
